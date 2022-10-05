9aee26a9d5296f57a836b803e7e154d479ae65a6
Emmanuel Salcido <salcidoe@amdocs.com>
2022-10-05 18:49:02 -0500

    init project

---
+++ src/main/java/com/service/appointmentme/AppointmentmeApplication.java
@@ -0,0 +1,13 @@
+package com.service.appointmentme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentmeApplication.class, args);
+	}
+
+}
